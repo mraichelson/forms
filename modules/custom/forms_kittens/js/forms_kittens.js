dabf140056cd3ac0dc98a7d8f69838a958a8179b
Michael Raichelson <i.am@michaelraichelson.com>
2012-11-08 16:43:50 -0500

    adding a custom JS behavior for the form to the demo.

--- modules/custom/forms_kittens/js/forms_kittens.js
+++ modules/custom/forms_kittens/js/forms_kittens.js
@@ -2,3 +2,12 @@
  * @file
  * Custom JS interactions for the kittens form.
  */
+
+Drupal.behaviors.forms_kittens = {
+  attach : function (context, settings) {
+    jQuery('#forms-kittens-form div.form-item').hover(
+      function () { jQuery(this).addClass('form-item-hover'); },
+      function () { jQuery(this).removeClass('form-item-hover'); }
+    );
+  }
+};
